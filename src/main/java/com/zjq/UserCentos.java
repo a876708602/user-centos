f5090a83907a3897aed049b6cd21634b4b5c92f0
a876708602 <876708602@qq.com>
2023-06-07 11:10:43 +0800

    添加数据源

--- src/main/java/com/zjq/UserCentos.java
+++ src/main/java/com/zjq/UserCentos.java
@@ -3,6 +3,9 @@ package com.zjq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author DELL
+ */
 @SpringBootApplication
 public class UserCentos {
     public static void main(String[] args) {
